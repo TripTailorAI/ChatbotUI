1475bf4751326cab1d3024918674f15c97cbc2cc
voyager-git <voyagerai.info@gmail.com>
2024-08-07 20:15:26 +0200

    Fixing error of parsing

--- revision0608.py
+++ revision0608.py
@@ -605,18 +605,38 @@ def generate_df(all_itineraries):
     columns = ['itinerary_version', 'date', 'weather', 'time', 'activity', 'place', 'MapsLink', 'Address', 'Hours', 'is_night']
     
     for i, itinerary in enumerate(all_itineraries, 1):
+        # Process day itinerary
         for day in itinerary['day']:
             for activity in day['activities']:
-                itinerary_data.append([i, day['date'], day['weather'], activity['time'], activity['activity'],
-                                       activity['place']['name'], activity['place']['url'], 
-                                       activity['place']['formatted_address'], activity['opening_hours'], False])
+                itinerary_data.append([
+                    i,
+                    day['date'],
+                    day['weather'],
+                    activity['time'],
+                    activity['activity'],
+                    activity['place']['name'],
+                    activity['place']['url'],
+                    activity['place']['formatted_address'],
+                    activity['opening_hours'],
+                    False  # is_night
+                ])
         
+        # Process night itinerary if it exists
         if itinerary['night']:
             for night in itinerary['night']:
                 for activity in night['activities']:
-                    itinerary_data.append([i, night['date'], night['weather'], activity['time'], activity['activity'],
-                                           activity['place']['name'], activity['place']['url'], 
-                                           activity['place']['formatted_address'], activity['opening_hours'], True])
+                    itinerary_data.append([
+                        i,
+                        night['date'],
+                        night['weather'],
+                        activity['time'],
+                        activity['activity'],
+                        activity['place']['name'],
+                        activity['place']['url'],
+                        activity['place']['formatted_address'],
+                        activity['opening_hours'],
+                        True  # is_night
+                    ])
     
     df = pd.DataFrame(itinerary_data, columns=columns)
     return df
