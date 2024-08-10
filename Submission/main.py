fe1a36c8daf3c6cb03455a83ebdae2e2d2d97519
voyager-git <voyagerai.info@gmail.com>
2024-08-10 16:13:34 +0200

    moved up

--- Submission/main.py
+++ Submission/main.py
@@ -26,6 +26,8 @@ from get_itinerary import get_daily_itinerary, get_nightlife_itinerary
 from output import create_itinerary_pdf, display_itinerary, generate_df, send_to_gsheets, getAccessToken, send_email
 from create_itinerary import create_travel_itinerary, create_night_itinerary
 from streamlit_page import streamlit_page
+streamlit_pageconfig()
+streamlit_page()
 
 GOOGLE_API_KEY = st.secrets['GOOGLE_API_KEY']
 genai.configure(api_key=GOOGLE_API_KEY)
@@ -34,5 +36,3 @@ weather_api_key = st.secrets['WEATHER']
 
 pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
 pdfmetrics.registerFont(TTFont('DejaVuSans-Bold', 'DejaVuSans-Bold.ttf'))
-streamlit_pageconfig()
-streamlit_page()
