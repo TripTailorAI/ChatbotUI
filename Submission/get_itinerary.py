c103e5ff849817aa33ba919c7bd7e966e3861003
voyager-git <voyagerai.info@gmail.com>
2024-08-10 17:36:13 +0200

    imports

--- Submission/get_itinerary.py
+++ Submission/get_itinerary.py
@@ -1,5 +1,27 @@
+from streamlit_page import streamlit_page, streamlit_pageconfig
+# from streamlit_config import streamlit_pageconfig
 import streamlit as st
+import requests
+import json
+import pandas as pd
+import random
 import google.generativeai as genai
+from datetime import datetime, timedelta, date
+import traceback
+import time
+import pycountry
+import pygsheets
+from google.oauth2.service_account import Credentials
+from google.oauth2 import service_account
+import google.auth.transport.requests
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, landscape
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from io import BytesIO
 
 GOOGLE_API_KEY = st.secrets['GOOGLE_API_KEY']
 genai.configure(api_key=GOOGLE_API_KEY)
