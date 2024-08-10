76f8fdcfe28520429eb801ed7c69341d025be1a9
voyager-git <voyagerai.info@gmail.com>
2024-08-10 16:14:30 +0200

    first

--- Submission/main.py
+++ Submission/main.py
@@ -1,3 +1,5 @@
+from streamlit_page import streamlit_page
+streamlit_pageconfig()
 import streamlit as st
 import requests
 import json
@@ -25,8 +27,6 @@ from place_weather import get_place_details, get_weather_forecast
 from get_itinerary import get_daily_itinerary, get_nightlife_itinerary
 from output import create_itinerary_pdf, display_itinerary, generate_df, send_to_gsheets, getAccessToken, send_email
 from create_itinerary import create_travel_itinerary, create_night_itinerary
-from streamlit_page import streamlit_page
-streamlit_pageconfig()
 streamlit_page()
 
 GOOGLE_API_KEY = st.secrets['GOOGLE_API_KEY']
