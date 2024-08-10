9bfb9cbf60a1026bca96baa9a0fd8754d943b801
voyager-git <voyagerai.info@gmail.com>
2024-08-10 17:10:29 +0200

    combined

--- Submission/main.py
+++ Submission/main.py
@@ -1,5 +1,5 @@
-from streamlit_page import streamlit_page
-from streamlit_config import streamlit_pageconfig
+from streamlit_page import streamlit_page, streamlit_pageconfig
+# from streamlit_config import streamlit_pageconfig
 import streamlit as st
 import requests
 import json
@@ -27,7 +27,9 @@ from get_itinerary import get_daily_itinerary, get_nightlife_itinerary
 from output import create_itinerary_pdf, display_itinerary, generate_df, send_to_gsheets, getAccessToken, send_email
 from create_itinerary import create_travel_itinerary, create_night_itinerary
 st.set_page_config(layout="wide")
+
 streamlit_pageconfig()
+
 streamlit_page()
 
 GOOGLE_API_KEY = st.secrets['GOOGLE_API_KEY']
--- Submission/streamlit_page.py
+++ Submission/streamlit_page.py
@@ -1,5 +1,51 @@
 import streamlit as st
 import pycountry
+from datetime import datetime, timedelta, date
+
+@st.cache_data(ttl=3600)
+def streamlit_pageconfig():
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+
+    if 'generated_itineraries' not in st.session_state:
+        st.session_state.generated_itineraries = None
+
+    if 'all_generated_itineraries' not in st.session_state:
+        st.session_state.all_generated_itineraries = []
+
+    if 'itinerary_set_count' not in st.session_state:
+        st.session_state.itinerary_set_count = 0
+
+    if 'custom_preferences' not in st.session_state:
+        st.session_state.custom_preferences = ""
+
+    if 'email_address' not in st.session_state:
+        st.session_state.email_address = ""
+
+    if 'country' not in st.session_state:
+        st.session_state.country = ""
+    if 'destination' not in st.session_state:
+        st.session_state.destination = ""
+        
+    if 'hotel_name' not in st.session_state:
+        st.session_state.hotel_name = ""
+        
+    if 'start_date' not in st.session_state:
+        st.session_state.start_date = date.today() + timedelta(days=1)
+        
+    if 'end_date' not in st.session_state:
+        st.session_state.end_date = date.today() + timedelta(days=1)
+        
+    if 'purpose_of_stay' not in st.session_state:
+        st.session_state.purpose_of_stay = "Vacation"
+        
+    if 'mode_of_transport' not in st.session_state:
+        st.session_state.mode_of_transport = "🚗 Driving"
+        
+    if 'custom_preferences' not in st.session_state:
+        st.session_state.custom_preferences = ""
+
+    # List of all countries
 
 countries = sorted([country.name for country in pycountry.countries])
 @st.cache_data(ttl=3600)
@@ -126,8 +172,7 @@ def streamlit_page():
         # Display the most recently generated itinerary set
         most_recent_set = st.session_state.all_generated_itineraries[-1]
         st.write("## Most Recent Itineraries")
-        
-        if isinstance(most_recent_set, dict):
+       if isinstance(most_recent_set, dict):
             day_itineraries = most_recent_set.get('day', [])
             night_itineraries = most_recent_set.get('night') if st.session_state.generate_nightlife else None
         else:
