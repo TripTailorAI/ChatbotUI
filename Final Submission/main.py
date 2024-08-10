cbc296ff5c45f991370ff55099078f977c050405
voyager-git <voyagerai.info@gmail.com>
2024-08-10 14:39:40 +0200

    breakout

--- Final Submission/main.py
+++ Final Submission/main.py
@@ -20,7 +20,12 @@ from reportlab.lib.units import inch
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from io import BytesIO
-
+from streamlit_config import streamlit_pageconfig
+from place_weather import get_place_details, get_weather_forecast
+from get_itinerary import get_daily_itinerary, get_nightlife_itinerary
+from output import create_itinerary_pdf, display_itinerary, generate_df, send_to_gsheets, getAccessToken, send_email
+from create_itinerary import create_travel_itinerary, create_night_itinerary
+from streamlit_page import streamlit_page
 
 GOOGLE_API_KEY = st.secrets['GOOGLE_API_KEY']
 genai.configure(api_key=GOOGLE_API_KEY)
@@ -30,19 +35,6 @@ weather_api_key = st.secrets['WEATHER']
 pdfmetrics.registerFont(TTFont('DejaVuSans', 'DejaVuSans.ttf'))
 pdfmetrics.registerFont(TTFont('DejaVuSans-Bold', 'DejaVuSans-Bold.ttf'))
 
-from streamlit_config import streamlit_pageconfig
 streamlit_pageconfig()
-
-from place_weather import get_place_details
-from place_weather import get_weather_forecast
 @st.cache_data(ttl=3600)
-
-from get_itinerary import get_daily_itinerary
-from get_itinerary import get_nightlife_itinerary
-from output import create_itinerary_pdf
-from output import display_itinerary
-from output import generate_df, send_to_gsheets, getAccessToken, send_email
-from create_itinerary import create_travel_itinerary
-from create_itinerary import create_night_itinerary
-from streamlit_page import streamlit_page
 streamlit_page()
