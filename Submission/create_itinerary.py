ee2519441175545cff11dc1cd300aa3f2416f5ae
voyager-git <voyagerai.info@gmail.com>
2024-08-10 19:07:58 +0200

    @st.cache(show_spinner=False)

--- Submission/create_itinerary.py
+++ Submission/create_itinerary.py
@@ -28,8 +28,8 @@ from place_weather import get_place_details, get_weather_forecast
 from get_itinerary import get_daily_itinerary, get_nightlife_itinerary, is_place_in_location, get_place_opening_hours
 from output import create_itinerary_pdf, display_itinerary, generate_df, send_to_gsheets, getAccessToken, send_email
 
-@st.cache(show_spinner=False)
 @st.cache_data(ttl=3600)
+@st.cache(show_spinner=False)
 def create_travel_itinerary(destination, country, start_date, end_date, hotel_name, purpose_of_stay, mode_of_transport, custom_preferences):
     weather_forecast_data = get_weather_forecast(destination)
     num_days = (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days + 1
