7f8b7c2194c55d1ec0f7518230e11858656dddce
voyager-git <voyagerai.info@gmail.com>
2024-08-11 15:27:19 +0200

    button lines

--- Submission/streamlit_page.py
+++ Submission/streamlit_page.py
@@ -138,6 +138,14 @@ def streamlit_page():
         key="generate_nightlife"
     )
 
+    col1, col2, col3 = st.columns([1,1,1])
+
+    with col1:
+        st.button('1')
+    with col2:
+        st.button('2')
+    with col3:
+        st.button('3')
 
     if st.sidebar.button("✍ Generate Itineraries"):
         with st.spinner("Generating itinerary, please wait..."):
